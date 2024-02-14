e4b1f2ccfce793b659ab8695f681ade57852cfe0
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-02-14 10:12:09 +0700

    FIX: RESPONSIVE AUTH PAGE

--- components/fragments/AuthCard.tsx
+++ components/fragments/AuthCard.tsx
@@ -14,14 +14,14 @@ const AuthCard = (props: Props) => {
           initial={{ opacity:0,translateX: -100, }}
           animate={{ opacity:1,translateX: 0, }}
           transition={{ duration: 0.5,delay: 0.1 }}
-        className='rounded-lg overflow-hidden w-1/2'>
+        className='rounded-lg overflow-hidden w-1/2 hidden md:block'>
             <Image className='object-cover w-full' width={300} height={200} src={props.imgUrl} alt='img auth'/>
         </motion.div>
         <motion.div
          initial={{ opacity:0,translateX: -100, }}
          animate={{ opacity:1,translateX: 0, }}
          transition={{ duration: 0.5 }}
-        className='bg-base-300 w-1/2 rounded p-2 flex flex-col justify-center items-center'>
+        className='bg-base-300 w-full p-5 md:w-1/2 md:p-2 gap-5 md:gap-0 rounded flex flex-col justify-center items-center'>
             {props.children}
         </motion.div>
     </div>
