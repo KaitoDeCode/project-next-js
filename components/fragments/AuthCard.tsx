e7a71aa9e57b270b351d791123481258be14b065
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-02-14 09:24:19 +0700

    REFACTOR: SIGNIN PAGE

--- components/fragments/AuthCard.tsx
+++ components/fragments/AuthCard.tsx
@@ -9,11 +9,11 @@ type Props = {
 
 const AuthCard = (props: Props) => {
   return (
-    <div className='m-auto flex'>
-        <div className='rounded-lg'>
-            <Image className='object-cover' width={300} height={200} src={props.imgUrl} alt='img auth'/>
+    <div className='m-auto flex w-[500]'>
+        <div className='rounded-lg overflow-hidden w-1/2'>
+            <Image className='object-cover w-full' width={300} height={200} src={props.imgUrl} alt='img auth'/>
         </div>
-        <div>
+        <div className='bg-base-300 w-1/2 rounded p-2 flex flex-col justify-center items-center'>
             {props.children}
         </div>
     </div>
---
+++ components/fragments/AuthCardContent.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import React, { FormEventHandler } from 'react'
+
+type Props = {
+    title: string,
+    intro: string,
+    children: React.ReactNode,
+    onSubmit: FormEventHandler<HTMLFormElement>,
+}
+
+const AuthCardContent = (props: Props) => {
+    const{
+        title,
+        intro,
+        children,
+        onSubmit
+    }= props
+  return (
+    <>
+        <h1 className='text-primary font-bold text-3xl'>{title}</h1>
+        <p className='mb-4 mt-1 font-light text-accent-content'>{intro}</p>
+        <form onSubmit={onSubmit} className='flex flex-col gap-3'>
+            {children}
+        </form>
+    </>
+  )
+}
+
+export default AuthCardContent
